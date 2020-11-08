68c967af2491914fed648ccabb587c74368d2344
moemi ohtsu <s0623192@gmail.com>
2020-11-08 23:15:48 +0900

    07.import

--- udemy/lesson.go
+++ udemy/lesson.go
@@ -1,21 +1,12 @@
 package main
 
-import "fmt"
-
-// Initは特別な関数名。
-// main関数よりも先に呼ばれる。
-func init() {
-  fmt.Println("Init!")
-}
-
-// オリジナル関数。このままだと実行されない。
-// main 関数の中で呼んであげる必要がある。
-func bazz() {
-  fmt.Println("Bazz")
-}
+import (
+  "fmt"
+  "os/user"
+  "time"
+)
 
 func main() {
-  bazz()
-  // fmt はフォーマットというライブラリ
-  fmt.Println("Hello World!", "TEST TEST")
+  fmt.Println("Hello World!", time.Now())
+  fmt.Println(user.Current())
 }
