eede133fc40b12ee8401a5b52860b565bbb9441b
moemi ohtsu <s0623192@gmail.com>
2020-11-10 23:52:53 +0900

    9. const

---
+++ udemy/09_const.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+const (
+  Username = "test_user"
+  Password = "test_pass"
+)
+
+// var big int = 9223372036854775807 + 1
+const big = 9223372036854775807 + 1
+
+func main() {
+  fmt.Println(Pi, Username, Password)
+//   Pi = 3
+  fmt.Println(big-1)
+}
