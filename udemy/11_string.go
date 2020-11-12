6950b26a4cca682f4556706c97f89963409c3b7a
moemi ohtsu <s0623192@gmail.com>
2020-11-12 23:40:39 +0900

    11. 文字列型

---
+++ udemy/11_string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+  fmt.Println("Hello " + "World")
+  fmt.Println("Hello World"[0]) // 72
+  fmt.Println(string("Hello World"[0]))
+}
