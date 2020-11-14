9eb9e05cd5ca2cf960de87d9c0fc3dcd1b41c67b
moemi ohtsu <s0623192@gmail.com>
2020-11-14 23:54:51 +0900

    11. 文字列型

--- udemy/11_string.go
+++ udemy/11_string.go
@@ -12,5 +12,18 @@ func main() {
   fmt.Println(string("Hello World"[0]))
 
   var s string = "Hello World"
-  fmt.Println(strings.Replace(s, "H", "X", 1))
+  s = (strings.Replace(s, "H", "X", 1))
+  fmt.Println(s)
+  fmt.Println(strings.Contains(s, "World"))
+
+  // バッククオートで囲うと、エディタの改行がそのまま反映できる
+  fmt.Println(`Test
+Test`)
+
+  // ダブルクオートの中にダブルクオート描きたい場合は
+  // \（バックスラッシュ）でエスケープ
+  fmt.Println("\"")
+
+  // バックスラッシュめんどいときは、バッククオートでエスケープ
+  fmt.Println(`"`)
 }
