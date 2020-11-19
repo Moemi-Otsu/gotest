47bfb074bb629400b873f5adebabad5a452cbe7c
moemi ohtsu <s0623192@gmail.com>
2020-11-20 00:13:08 +0900

    17.map

--- udemy/17_map.go
+++ udemy/17_map.go
@@ -20,4 +20,32 @@ func main() {
 
   // mapに存在しないキーを指定すると 0 が返る
   fmt.Println(m["nothing"])
+
+  // 100 true 返り値がある場合はこのように２つ書ける??ここよくわからん
+  v, ok := m["apple"]
+  fmt.Println(v, ok)
+
+  // 0 false
+  v2, ok2 := m["nothing"]
+  fmt.Println(v2, ok2)
+
+  // m2["pc"] = 5000
+  m2 := make(map[string]int)
+  m2["pc"] = 5000
+  fmt.Println(m2)
+
+  // panic: assignment to entry in nil map
+  // 宣言はしているものの、メモリ上にmapがないのでエラーに
+  /*
+  var m3 map[string]int
+  m3["pc"] = 5000
+  fmt.Println(m3)
+  */
+
+  // var宣言した場合は、
+  // sliceのときでも、mapのときでもnilが返る。
+  var s []int
+  if s == nil{
+    fmt.Println("Nil")
+  }
 }
