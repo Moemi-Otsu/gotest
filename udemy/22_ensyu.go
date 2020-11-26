c724c4c2e1a9d96e2c32430dcd2b41994b0e7aa8
moemi ohtsu <s0623192@gmail.com>
2020-11-26 23:33:32 +0900

    22.演習

--- udemy/22_ensyu.go
+++ udemy/22_ensyu.go
@@ -4,5 +4,17 @@ import "fmt"
 
 func main() {
   f := 1.11
-  fmt.Println(string(f))
+  i := int(f)
+  fmt.Println(i)
+  fmt.Printf("%T %v\n", i, i)
+
+  s := []int{1, 2, 5, 6, 2, 3, 1}
+  fmt.Println(s[2:4])
+
+   m := map[string]int{
+    "Mike": 20,
+    "Nancy": 24,
+    "Messi": 30,
+   }
+   fmt.Printf("%T %v", m, m)
 }
