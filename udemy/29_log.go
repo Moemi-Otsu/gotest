c1d04318c6755e3db806d2d797ffb7df75db2f8b
moemi ohtsu <s0623192@gmail.com>
2020-12-05 00:43:48 +0900

    29.log

--- udemy/29_log.go
+++ udemy/29_log.go
@@ -1,8 +1,28 @@
 package main
 
-import "log"
+import (
+  "log"
+  "fmt"
+  "os"
+  "io"
+)
+
+func LoggingSettings(logFile string){
+  // パイプ|を使って複数の条件を書ける
+  // 0666はパーミッション
+  logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+  multiLogFile := io.MultiWriter(os.Stdout, logfile)
+  log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+  log.SetOutput(multiLogFile)
+}
 
 func main(){
+  LoggingSettings("test.log")
+  _, err := os.Open("fdsfdfds")
+  if err != nil{
+    log.Fatalln("Exit", err)
+  }
+
   /*
   他の言語では、loggingライブラリにこんなのあったりする
   でも、goにはこれらは存在しない
@@ -15,4 +35,12 @@ func main(){
   */
 
   log.Println("logging!")
+  log.Printf("%T %v", "test", "test")
+
+  // フェイタルを使うと、その時点でコードが終了する
+  log.Fatalf("%T %v", "test", "test")
+  log.Fatalln("error!!")
+
+  // 実行されない
+  fmt.Println("ok!")
 }
